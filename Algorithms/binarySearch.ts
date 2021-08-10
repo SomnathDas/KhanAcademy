4420bdd384ff90cfee225ea39946c4a17e81d3f9
Somnath Das <37792778+SomnathDas@users.noreply.github.com>
2021-08-10 11:36:02 +0530

    Fixed

--- Algorithms/binarySearch.ts
+++ Algorithms/binarySearch.ts
@@ -3,28 +3,29 @@ const binarySearch = (array: [], targetValue: number): number => {
   let min: number = 0;
   // Declaring Maximum Search Range
   let max: number = array.length - 1;
-  // Declaring the guess
-  let guess: number;
+  // Declaring the middle value
+  let mid: number;
 
   /* Looping until it returns the result or atleast until 
   our max range becomes lower than or equal to min */
-  while (max <= min) {
-    /* Settings our guess variable as the average 
+  while (min <= max) {
+    /* Settings our mid variable as the average 
     between max and min range and rounding down
     to get an integer*/
-    guess = Math.floor((max + min) / 2);
-    /* If guess happens to locate the target value in
-    the array then we returns the target value*/
-    if (array[guess] === targetValue) {
-      return array[guess];
-      /* If guess happens to locate a value lesser than
-      the target value then we increase our minimum range*/
-    } else if (array[guess] < targetValue) {
-      min = targetValue + 1;
-    } else {
-      /* If guess happens to locate a value greater than
+    mid = Math.floor((max + min) / 2);
+    /* If mid happens to locate a value greater than
       the target value then we increase our maximum range*/
-      max = targetValue - 1;
+    if (targetValue < array[mid]) {
+      max = mid - 1;
+      /* If mid happens to locate a value lesser than
+      the target value then we increase our minimum range*/
+    } else if (targetValue > array[mid]) {
+      min = mid + 1;
+    } else {
+      /* If mid happens to locate the target value in
+    the array then we returns the mid value referring
+    to the position of that element in the array*/
+      return mid;
     }
   }
   // If target is not found in the array at the end
